7d5f4300c2f212dee05d17f7c26608f075b4a4c3
FENG <982685807@qq.com>
2023-11-01 17:31:25 +0800

    update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
