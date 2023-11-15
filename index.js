747a44e5ef4552a49bb5f2cfa5c9b891074578a7
FENG <982685807@qq.com>
2023-11-15 16:31:42 +0800

    add lint check code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors =require('cors')
+const cors = require('cors')
 const Note = require('./models/note')
 
 app.use(express.static('build'))
@@ -13,7 +13,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes =>{
+    Note.find({}).then(notes => {
         response.json(notes)
     })
 })
@@ -23,81 +23,79 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-app.get('/api/notes/:id',(request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
-    .then(note => {
-        if(note){
-            response.json(note)
-        }else{
-            response.status(404).end
-        }
-    })
-    .catch(error => next(error))
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id',(request,response,next) => {//暂时没有这个功能
+app.delete('/api/notes/:id', (request, response, next) => {//暂时没有这个功能
     Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(response.status(204).end())
+        .catch(error => next(error))
     // const id = Number(request.params.id)
     // notes = notes.filter(note => note.id !== id)
 
     // response.status(204).end()
 })
 
-app.post('/api/notes',(request,response,next) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
-    Note.findOne({content: body.content})
-    .catch(error => {
-        console.log(error)
-        return response.status(400).json({
-            error: 'commit same content, please correct it and try again'
+    Note.findOne({ content: body.content })
+        .catch(error => {
+            console.log(error)
+            return response.status(400).json({
+                error: 'commit same content, please correct it and try again'
+            })
         })
-    })
     const note = new Note({
         content: body.content,
-        important: body.important||false,
+        important: body.important || false,
         date: new Date(),
     })
 
     note.save().then(savedNote => {
         response.json(savedNote)
     })
-    .catch(error => next(error))
+        .catch(error => next(error))
 })
 
-app.put('/api/notes/:id',(request,response,next) => {
-    const {content, important} = request.body
-    
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
     Note.findByIdAndUpdate(request.params.id,
-        {content, important}, 
-        {new:true, runValidators:true, context:'query'})
-    .then(updatedNote => {
-        response.json(updatedNote)
-    })
-    .catch(error => next(error))
+        { content, important },
+        { new: true, runValidators: true, context: 'query' })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 //处理不支持路由的程序应该放在倒数第二个
 const unknowEndpoint = (request, response) => {
-    response.status(404).send({error:'unknow endpoint'})
+    response.status(404).send({ error: 'unknow endpoint' })
 }
 app.use(unknowEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
-    if(error.name === 'CastError'){
-        return response.status(400).send({error:'malformated id'})
-    }else if(error.name === 'No Content'){
-        return response.status(400).send({error: 'data already been deleted'})
-    }else if(error.name ==='ValidationError'){
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformated id' })
+    } else if (error.name === 'No Content') {
+        return response.status(400).send({ error: 'data already been deleted' })
+    } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
-    
+
     next(error)
 }
 // 这必须是最后一个载入的中间件。
--- models/note.js
+++ models/note.js
@@ -3,28 +3,26 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URL
 
 mongoose.connect(url)
-.then(result => {
-    console.log('connect to MongoDB')
-})
-.catch((error) =>{//error 为什么要用括号括起来
-    console.log('error connecting to MongoDB', error.message)
-})
+    .then(console.log('connect to MongoDB'))
+    .catch((error) => {//error 为什么要用括号括起来
+        console.log('error connecting to MongoDB', error.message)
+    })
 
 const noteSchema = new mongoose.Schema({
-    content:{
+    content: {
         type: String,
         minLength: 5, //内置
         required: true//内置
     },
-    date:{
+    date: {
         type: Date,
         required: true
     },
     important: Boolean
 })
 
-noteSchema.set('toJSON',{
-    transform:(document, returnedObject) => {
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
